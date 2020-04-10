e1fd3cbea474eac853e315e1d28bf6e339ddf915
shubhsherl <shubham.singh094@yahoo.com>
2020-04-10 16:43:53 +0530

    add crispy in book form

--- register/forms.py
+++ register/forms.py
@@ -9,4 +9,4 @@ class RegisterForm(UserCreationForm):
 
 	class Meta:
 		model = User
-		fields = ["username", "email", "password1", "password2"]
+		fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
